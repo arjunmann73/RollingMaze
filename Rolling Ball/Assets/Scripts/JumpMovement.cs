19f33861d38c8b72124171a58f0b8bf9d3ffad99
Arjun Singh <arjunmann73@gmail.com>
2020-10-27 00:10:01 +0800

    Added jump movements

---
+++ Rolling Ball/Assets/Scripts/JumpMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpMovement : MonoBehaviour
+{
+
+    private float jumpSpeed = 5;
+    private Rigidbody rigidBody;
+    private bool onGround = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space") && onGround){
+            rigidBody.AddForce(Vector3.up * jumpSpeed, ForceMode.Impulse);
+            onGround = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision collision){
+        onGround = true;
+    }
+}
