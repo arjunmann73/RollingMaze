0e6c562461c57b4dfbad31d637991a0802fb835a
Arjun Singh <arjunmann73@gmail.com>
2020-10-26 14:47:00 +0800

    Initialised Repo

---
+++ Rolling Ball/Assets/Scripts/ZoneTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoneTrigger : MonoBehaviour
+{
+   
+   private void OnTriggerEnter(Collider other){
+       
+       if (other.CompareTag("Sphere")){
+           print("Player entered a wrong area!");
+       }
+   }
+
+
+}
