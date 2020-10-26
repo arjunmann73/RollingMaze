942f6e23ba837123c264df8d0d542ab6446fd4fc
Arjun Singh <arjunmann73@gmail.com>
2020-10-26 16:28:39 +0800

    Added boundaries and player movements

---
+++ Rolling Ball/Assets/Scripts/RollingBall.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollingBall : MonoBehaviour
+{
+    public Rigidbody obj;
+    public float hor;
+    public float ver;
+    public float speed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hor = Input.GetAxis("Horizontal");
+        ver = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(hor, 0.0f, ver);
+
+        obj.AddForce(direction * speed);
+    }
+}
