a361801a90e6d6022bfd75e9eaf9cca6dd59563d
Arjun Singh <arjunmann73@gmail.com>
2020-11-07 21:42:39 +0800

    Added floor is lava, boundaries etc.

---
+++ Rolling Ball/Assets/Scripts/ConstantMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantMovement : MonoBehaviour
+{
+    private float Sign = -1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Sign * Time.deltaTime * 2 * transform.forward;
+        if (transform.position.z > 6f){
+             Sign = -1f;
+        } 
+        else if (transform.position.z < 0f){
+             Sign = 1f;
+        }   
+    }
+}
---
+++ Rolling Ball/Assets/Scripts/CountDownTimer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    float currentTime = 0f;
+    float startingTime = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+    }
+}
